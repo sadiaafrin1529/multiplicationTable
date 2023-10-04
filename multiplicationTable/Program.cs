d3044c185f8e1cd17e186bfbf4d6566c83ed53e6
sadiaafrin1529 <sadiaafrin1529@gmail.com>
2023-10-05 01:23:25 +0600

    Add project files.

---
+++ multiplicationTable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, result;
+            Console.Write("Enter the number : ");
+            x = Convert.ToInt32(Console.ReadLine());
+            for (int i=0; i<11; i++)
+            {
+                result = x * i;
+                Console.WriteLine("{0} * {1} = {2}", x, i, result);
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
